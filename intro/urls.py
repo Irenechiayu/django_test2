77c52e701ef096cc568b303d1e0a614eeeff6201
Jeffrey L. Eppinger <jle@cs.cmu.edu>
2017-01-28 21:52:08 -0500

    Added code for intro to be app in larger site.
    Created intro/urls.py.  Made webapps/urls.py include intro/urls.py.
    Changed linked to static css file to have absolute URL.

--- intro/urls.py
+++ intro/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    
-    url(r'^intro/', include('intro.urls')),
+    url(r'^$', views.home_page),
+    url(r'^hello-world$', views.hello_world),
+    url(r'^hello-world-with-template$', views.hello_world_with_template),
+    url(r'^greet$', views.greet),
 ]
